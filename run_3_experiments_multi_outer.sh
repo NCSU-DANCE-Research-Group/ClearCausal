5d6b7e3f4a3192a37d401003299ad7cc56b65ad6
Fogo Tunde-Onadele <fogijay@yahoo.co.uk>
2024-07-14 16:40:50 -0400

    Fix the outer run script - assignment, variable name

--- run_3_experiments_multi_outer.sh
+++ run_3_experiments_multi_outer.sh
@@ -2,11 +2,11 @@ now=$(date)
 
 # 1. bug IDs - choose bugs
 bug_ids=("1")  # ("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
-num_bugs=$((${#bugids[@]} - 1))
+num_bugs=$((${#bug_ids[@]} - 1))
 
 # 2. choose service or function analysis
 # True: perform root cause function analysis, False: perform root cause service analysis
-function_mode = "False"
+function_mode="False"
 
 # 3. update task in run_3_experiments_multi.sh which is used to name exp folders 
 task_name="fca_service" # mi mi_ad microscope fca service function 
@@ -24,10 +24,10 @@ do
     # Change line 5 in setting.py to look like 'BUGID = bug_id'
     sed -i "${setting_line_bug}s/.*/BUGID = $bug_id/" setting.py
 	
-	# Change line 16 in setting.py to look like 'ROOT_CAUSE_FUNCTION_ANALYSIS = function_mode'
+    # Change line 16 in setting.py to look like 'ROOT_CAUSE_FUNCTION_ANALYSIS = function_mode'
     sed -i "${setting_line_function_mode}s/.*/ROOT_CAUSE_FUNCTION_ANALYSIS = $function_mode/" setting.py
 
-	# Run experiment for bug_id
+    # Run experiment for bug_id
     ./run_3_experiments_multi.sh "$bug_id" "$task_name"   
+    echo "run_3_experiments_multi.sh $bug_id $task_name"
 done
-
